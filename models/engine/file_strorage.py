757a003896a384ffda405fcf67b3aa7d16627858
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2022-09-17 19:56:58 -0700

    added file

--- models/engine/file_strorage.py
+++ models/engine/file_strorage.py
@@ -39,20 +39,20 @@ class FileStorage:
         from models.review import Review
 
         classes = {
-                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
-                    'State': State, 'City': City, 'Amenity': Amenity,
-                    'Review': Review
-                  }
+            'BaseModel': BaseModel, 'User': User, 'Place': Place,
+            'State': State, 'City': City, 'Amenity': Amenity,
+            'Review': Review
+        }
         try:
             temp = {}
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
 
-     def delete(self, obj=None):
+    def delete(self, obj=None):
         """Delete object from file storage"""
         if obj is not None:
             for k, v in self.__objects.copy().items():
