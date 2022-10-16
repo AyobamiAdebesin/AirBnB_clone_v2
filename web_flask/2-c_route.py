7c6976cf3cd64049288ad35d7a5b73345b419969
AyobamiAdebesin <froshofficial@gmail.com>
2022-10-17 00:31:25 +0100

    Added new script

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+A script that starts a Flask web application
+Listening on 0.0.0.0 port 5000
+
+Displays Hello HBNB on route "/"
+Displays HBNB on route "/hbnb"
+"""
+
+from flask import Flask
+from flask import escape0
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def say_hello():
+    """ Displas Hello HBNB """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def say_hbnb():
+    """ Displays HBNB """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_c():
+    return f"C {escape(text}"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
