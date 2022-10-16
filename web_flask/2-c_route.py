e9d6434185160c32a0d8b8ac480d3d0fda30b5e0
AyobamiAdebesin <froshofficial@gmail.com>
2022-10-17 00:46:05 +0100

    [Refactor]: fixed underscore replacement

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -24,11 +24,11 @@ def say_hbnb():
     return "HBNB"
 
 
-@app.route('/c/<text>', strict_slashes=False)
-def display_c(text):
-    """ Returns a custom message """
-    text = text.replace("_", " ") 
-    return f"C {text}"
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """Displays 'C' followed by the value of <text>."""
+    text = text.replace("_", " ")
+    return "C {}".format(text)
 
 
 if __name__ == "__main__":
