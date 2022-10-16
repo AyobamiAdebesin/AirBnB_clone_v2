e548edb8918a9f487dffac7f8b3b60d81c5e1d65
AyobamiAdebesin <froshofficial@gmail.com>
2022-10-17 00:19:26 +0100

    [Refactor]: changed code to meet requirement

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def say_hello():
     """ Displas Hello HBNB """
-    return "Hello HBNB"
+    return "Hello HBNB!"
 
 
 if __name__ == "__main__":
