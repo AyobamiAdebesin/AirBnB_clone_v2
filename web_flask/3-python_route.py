18009c5c1bfe783a16ea9f984a42d84fb6c2f1ae
AyobamiAdebesin <froshofficial@gmail.com>
2022-10-17 00:52:53 +0100

    First attempt

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""
+A script that starts a Flask web application
+Listening on 0.0.0.0 port 5000
+
+Displays Hello HBNB on route "/"
+Displays HBNB on route "/hbnb"
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def say_hello():
+    """ Displas Hello HBNB """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def say_hbnb():
+    """ Displays HBNB """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """Displays 'C' followed by the value of <text>."""
+    text = text.replace("_", " ")
+    return "C {}".format(text)
+
+
+@app.route("/python/<text>", strict_slashes=False)
+def display_python(text="so cool"):
+    """
+    Displays Python followed by the value of <text>
+    The default value is "is cool"
+    """
+    text = text.replace("_", " ")
+    return f"Python {text}"
+
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
