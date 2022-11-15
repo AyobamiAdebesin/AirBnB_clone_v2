8389deb0dd55e41d68e7b0ce890ca11a9bea57b5
AyobamiAdebesin <froshofficial@gmail.com>
2022-11-15 07:59:49 +0100

    Modified IP addresses of web servers

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -7,7 +7,7 @@ distributes an archive to the web servers
 from fabric.api import env, local, put, run
 from datetime import datetime
 from os.path import exists, isdir
-env.hosts = ['54.158.198.144', '54.237.35.160']
+env.hosts = ['34.205.65.146', '54.237.35.160']
 
 
 def do_pack():
