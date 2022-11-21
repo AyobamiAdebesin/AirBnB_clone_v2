512cea4c1dbeff02cb54e95313737bdf98d1170a
AyobamiAdebesin <froshofficial@gmail.com>
2022-11-21 02:42:43 +0100

    Fixed pycodestyle errors

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python3
+""" Starts a Flask web application
+
+The application listens on 0.0.0.0 port 5000
+Routes:
+    /cities_by_states: HTML page with a list of all states and related cities
+"""
+
 from flask import Flask
 from models import storage
 from flask import render_template
 from models.state import State
 app = Flask(__name__)
+
 
 @app.route("/cities_by_states", strict_slashes=False)
 def get_cities_by_states():
@@ -12,9 +20,11 @@ def get_cities_by_states():
             "8-cities_by_states.html",
             states=all_states)
 
+
 @app.teardown_appcontext
 def teardown(exc):
     storage.close()
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
