3abc38ddb90901d444debe333f76016eb190fd75
AyobamiAdebesin <froshofficial@gmail.com>
2022-12-04 21:51:17 +0100

    Enabled lazy loading

--- models/state.py
+++ models/state.py
@@ -12,7 +12,7 @@ class State(BaseModel, Base):
     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
         name = Column(String(128), nullable=False)
         cities = relationship(
-            "City", cascade="all, delete, delete-orphan", backref="states")
+            "City", cascade="all, delete, delete-orphan", backref="states", lazy="joined")
     else:
         name = ""
 
