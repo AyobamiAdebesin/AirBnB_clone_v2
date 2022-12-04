1b4829c6816c82e5408e7f91d1833854a0498487
AyobamiAdebesin <froshofficial@gmail.com>
2022-12-04 22:43:43 +0100

    New task

---
+++ web_flask/9-states.py
@@ -0,0 +1,108 @@
+#!/usr/bin/python3
+"""
+A script that starts a Flask web application
+Listening on 0.0.0.0 port 5000
+
+Displays Hello HBNB on route "/"
+Displays HBNB on route "/hbnb"
+"""
+
+from flask import Flask, abort, render_template
+from models import State
+from models import storage
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def say_hello():
+    """ Displas Hello HBNB """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def say_hbnb():
+    """ Displays HBNB """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """Displays 'C' followed by the value of <text>."""
+    text = text.replace("_", " ")
+    return "C {}".format(text)
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python(text="is cool"):
+    """
+    Displays Python followed by the value of <text>
+    The default value is "is cool"
+    """
+    text = text.replace("_", " ")
+    return "Python {}".format(text)
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    """ Displays n is a number"""
+    return "{} is a number".format(n)
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def number_template(n):
+    """ Displays a HTML page if n is an integer """
+    return render_template("5-number.html", n=n)
+
+
+@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
+def odd_or_even(n):
+    """Displays an HTML page only if n is an integer"""
+    return render_template("6-number_odd_or_even.html", n=n)
+
+
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    """
+    Displays a HMTL page with the list of all State objects present
+    in DBStorage sorted by name (A->Z)
+    """
+    my_db = storage.all(State)
+    states = []
+    for state in my_db.values():
+        states.append(state)
+    return render_template("7-states_list.html", states=states)
+
+
+@app.route("/cities_by_states", strict_slashes=False)
+def cities_by_states():
+    """Displays an HTML page with a list of all states and related cities.
+    States/cities are sorted by name.
+    """
+    states = storage.all(State)
+    return render_template("8-cities_by_states.html", states=states)
+
+
+@app.route("/states", strict_slashes=False)
+def get_all_states():
+    my_db = storage.all(State)
+    return render_template("9-states.html", states=my_db)
+
+
+@app.route("/states/<id>", strict_slashes=False)
+def get_state_by_id(id):
+    for state in storage.all(State).values():
+        if state.id == id:
+            return render_template("9-states.html", states=state)
+        return render_template("9-states.html")
+
+
+@app.teardown_appcontext
+def teardown(exc):
+    """Remove the current SQLAlchemy session."""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
